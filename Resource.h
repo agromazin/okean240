2d461db98ac8da64ec1cdd0ed660c9c287bb4689
Andrey Gromazin <argo13@gmail.com>
2023-11-10 19:48:00 +0300

    lost files

---
+++ Resource.h
@@ -0,0 +1,37 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by OkeanEmu.rc
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_OKEANEMU_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_OKEANEMU			107
+#define IDI_SMALL				108
+#define IDC_OKEANEMU			109
+#define IDC_OKEANEMU_MONITOR	110
+#define IDC_OKEANEMU_OS			111
+#define IDC_START_OKEAN			112
+#define IDC_RECORD_PLAYER		113
+#define IDC_FILE_FOR_PLAYER		114
+#define IDC_PLAYER_START		115
+
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
